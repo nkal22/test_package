3cd65b78361424bff6765a61b8e82c73516b4f39
Nick Kalinowski <72101251+nkal22@users.noreply.github.com>
2022-07-11 10:28:55 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='smiley',
+      version='0.1',
+      description='I love books',
+      url='https://github.com/nkal22/test_package/',
+      author='Nick Kalinowski AND Theo Thormann',
+      author_email='njk5yr@virginia.edu OR nbx5kp@virginia.edu',
+      license='MIT',
+      packages=['smiley']
+     )
